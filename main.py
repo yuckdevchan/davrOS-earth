5aa2b6dea2768c5bf0a1bf79133da251fb52abcf
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-06-20 13:00:32 +0100

    basic stuff

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 planet = int(input("planet:"))
 year = int(input("year: "))
+
+tests = ("population", "human progress")
+
+population = ("size", "life expectancy", "birth rate", "death rate")
+
+def test_list():
+  print("test categories:")
+  for i in tests:
+    print(i+1, tests[i])
+  print("end of category list.")
+  user_test_category = int(input("test category (e.g: 1)"))
+
+def test_list_sub_categories():
+  
+  
